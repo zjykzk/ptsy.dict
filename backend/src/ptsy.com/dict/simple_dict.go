92bdffd197656980040c02aaed5226348d36e6f5
zjykzk <zjykzk@gmail.com>
2018-02-17 14:50:43 +0800

    simple dict

---
+++ backend/src/ptsy.com/dict/simple_dict.go
@@ -0,0 +1,170 @@
+package dict
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+	"sync"
+)
+
+// Word the item in the dict
+type Word struct {
+	ID       int32  `json:"id"`
+	Ch       string `json:"ch"`
+	En       string `json:"en"`
+	Examples string `json:"examples"`
+	Source   string `json:"source"`
+	Origin   string `json:"origin"`
+	Position string `json:"Position"`
+	Comment  string `json:"Comment"`
+}
+
+// SimpleDict the simplest implemation of the dict
+type SimpleDict struct {
+	sync.RWMutex
+	words    []Word
+	filename string
+	logger   *log.Logger
+}
+
+// Add add one word
+func (d *SimpleDict) Add(word *Word) error {
+	d.Lock()
+	id := int32(0)
+	if len(d.words) > 0 {
+		id = d.words[len(d.words)-1].ID + 1
+	}
+	word.ID = id
+	d.words = append(d.words, *word)
+	d.Unlock()
+
+	return nil
+}
+
+// Update update one word
+func (d *SimpleDict) Update(word *Word) (err error) {
+	d.Lock()
+	i, exist := d.findIndex(word.ID)
+	if !exist {
+		err = errors.New("no word")
+		goto END
+	}
+	d.words[i] = *word
+END:
+	d.Unlock()
+	return
+}
+
+// Search search the words
+func (d *SimpleDict) Search(keyword string) ([]*Word, error) {
+	words := make([]*Word, 0, 32)
+	d.RLock()
+	for i := range d.words {
+		w := &d.words[i]
+		if strings.Contains(w.Ch, keyword) || strings.Contains(w.Comment, keyword) ||
+			strings.Contains(w.En, keyword) || strings.Contains(w.Examples, keyword) ||
+			strings.Contains(w.Origin, keyword) || strings.Contains(w.Position, keyword) ||
+			strings.Contains(w.Source, keyword) {
+			words = append(words, w)
+		}
+	}
+	d.RUnlock()
+	return words, nil
+}
+
+// Delete delete one word
+func (d *SimpleDict) Delete(id int32) (err error) {
+	d.Lock()
+	i, exist := d.findIndex(id)
+	if !exist {
+		err = errors.New("no word")
+		goto END
+	}
+
+	copy(d.words[i:], d.words[i+1:])
+	d.words = d.words[:len(d.words)-1]
+
+END:
+	d.Unlock()
+	return
+}
+
+// Find returns the words satify the conditions
+func (d *SimpleDict) Find(w *Word, offset, limit int) ([]*Word, int, error) {
+	ws := make([]*Word, 0, limit)
+
+	for i := range d.words {
+		wi := &d.words[i]
+		if (w.Ch != "" && strings.Contains(wi.Ch, w.Ch)) ||
+			(w.Comment != "" && strings.Contains(wi.Comment, w.Comment)) ||
+			(w.En != "" && strings.Contains(wi.En, w.En)) ||
+			(w.Examples != "" && strings.Contains(wi.Examples, w.Examples)) ||
+			(w.Origin != "" && strings.Contains(wi.Origin, w.Origin)) ||
+			(w.Position != "" && strings.Contains(wi.Position, w.Position)) ||
+			(w.Source != "" && strings.Contains(wi.Source, w.Source)) {
+			if offset <= 0 {
+				ws = append(ws, wi)
+				if len(ws) >= limit {
+					break
+				}
+			} else {
+				offset--
+			}
+		}
+	}
+
+	return ws, len(d.words), nil
+}
+
+// Start starts the dict
+func (d *SimpleDict) Start() error {
+	if _, err := os.Stat(d.filename); os.IsNotExist(err) {
+		return nil
+	}
+
+	data, err := ioutil.ReadFile(d.filename)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(data, &d.words)
+}
+
+// Shutdown shutdown the dict
+func (d *SimpleDict) Shutdown() {
+	data, err := json.Marshal(d.words)
+	if err != nil {
+		d.logger.Printf("[ERROR] marshal data error:%s", err)
+		return
+	}
+
+	if err = ioutil.WriteFile(d.filename, data, os.ModePerm); err != nil {
+		d.logger.Printf("[ERROR] write file %s error:%s", d.filename, err)
+	}
+}
+
+func (d *SimpleDict) findIndex(id int32) (i int32, exist bool) {
+	b, e := 0, len(d.words)-1
+	for b <= e {
+		m := (b + e) >> 1
+		mid := d.words[m].ID
+		switch {
+		case mid == id:
+			i, exist = int32(m), true
+			return
+		case mid < id:
+			b = m + 1
+		default:
+			e = m - 1
+		}
+	}
+	return
+}
+
+// NewSimpleDict creates dict using simplest style
+func NewSimpleDict() *SimpleDict {
+	return &SimpleDict{filename: "dict.json"}
+}
---
+++ backend/src/ptsy.com/dict/simple_dict_test.go
@@ -0,0 +1,133 @@
+package dict
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSimpleDict(t *testing.T) {
+	d := &SimpleDict{words: make([]Word, 0, 32)}
+
+	t.Run("add", func(t *testing.T) {
+		d.Add(&Word{Ch: "ch", En: "en"})
+		assert.Equal(t, 1, len(d.words))
+		assert.Equal(t, int32(0), d.words[0].ID)
+		d.Add(&Word{Ch: "ch", En: "en"})
+		assert.Equal(t, 2, len(d.words))
+		assert.Equal(t, int32(1), d.words[1].ID)
+	})
+
+	t.Run("update", func(t *testing.T) {
+		d.words = nil
+		d.Add(&Word{Ch: "ch", En: "en"})
+		d.Add(&Word{Ch: "ch", En: "en"})
+
+		w := &d.words[1]
+
+		w.Examples = "examples"
+		err := d.Update(w)
+		assert.Nil(t, err)
+
+		assert.Equal(t, "examples", d.words[1].Examples)
+
+		w.ID = -1
+		err = d.Update(w)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("delete", func(t *testing.T) {
+		d.words = nil
+		d.Add(&Word{Ch: "ch", En: "en"})
+		d.Add(&Word{Ch: "ch", En: "en"})
+		d.Add(&Word{Ch: "ch", En: "en"})
+
+		err := d.Delete(1)
+		assert.Nil(t, err)
+		assert.Equal(t, 2, len(d.words))
+		assert.Equal(t, int32(0), d.words[0].ID)
+		assert.Equal(t, int32(2), d.words[1].ID)
+		err = d.Delete(1)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("search", func(t *testing.T) {
+		d.words = nil
+		d.Add(&Word{Ch: "a1"})
+		d.Add(&Word{En: "b1"})
+		d.Add(&Word{Examples: "c1"})
+		d.Add(&Word{Source: "d1"})
+		d.Add(&Word{Origin: "e1"})
+		d.Add(&Word{Position: "f1"})
+		d.Add(&Word{Comment: "g1"})
+
+		ws, err := d.Search("1")
+		assert.Nil(t, err)
+		assert.Equal(t, 7, len(ws))
+
+		for i := range ws {
+			w := ws[i]
+			assert.Equal(t, int32(i), w.ID)
+		}
+
+		ws, err = d.Search("a")
+		assert.Nil(t, err)
+		assert.Equal(t, 1, len(ws))
+		assert.Equal(t, int32(0), ws[0].ID)
+
+		ws, err = d.Search("g1")
+		assert.Nil(t, err)
+		assert.Equal(t, 1, len(ws))
+		assert.Equal(t, int32(6), ws[0].ID)
+	})
+
+	t.Run("find", func(t *testing.T) {
+		d.words = nil
+		d.Add(&Word{Ch: "a1"})
+		d.Add(&Word{En: "b1"})
+		d.Add(&Word{Examples: "c1"})
+		d.Add(&Word{Source: "d1"})
+		d.Add(&Word{Origin: "e1"})
+		d.Add(&Word{Position: "f1"})
+		d.Add(&Word{Comment: "g1"})
+
+		ws, total, _ := d.Find(&Word{Ch: "1", Origin: "e"}, 1, 2)
+		assert.Equal(t, 7, total)
+		assert.Equal(t, 1, len(ws))
+		assert.Equal(t, int32(4), ws[0].ID)
+
+		ws, total, _ = d.Find(&Word{Ch: "1", Origin: "e"}, 2, 2)
+		assert.Equal(t, 7, total)
+		assert.Equal(t, 0, len(ws))
+	})
+
+	t.Run("start shutdown", func(t *testing.T) {
+		fn := "test_start"
+		ioutil.TempFile("", fn)
+		defer os.Remove(fn)
+
+		ioutil.WriteFile(fn, []byte(`[{"ch":"c","en":"en"},{"ch":"c1"}]`), os.ModePerm)
+
+		d := &SimpleDict{filename: fn}
+		d.Start()
+		assert.Equal(t, 2, len(d.words))
+
+		assert.Equal(t, "c", d.words[0].Ch)
+		assert.Equal(t, "en", d.words[0].En)
+		assert.Equal(t, "c1", d.words[1].Ch)
+
+		d.Add(&Word{Ch: "c3"})
+
+		d.Shutdown()
+
+		data, err := ioutil.ReadFile(fn)
+		var ws []Word
+		err = json.Unmarshal(data, &ws)
+		assert.Nil(t, err)
+
+		assert.Equal(t, 3, len(ws))
+	})
+}
